a0a6293ef0c046588598f90226146a82a25acf3a
Brennan Butler <bbutler522@gmail.com>
2021-05-31 10:05:47 -0400

    Set up work data routing

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,22 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.scss'
 import { getSortedPostsData } from '../lib/posts'
+import { getSortedWorkData } from '../lib/work'
 import Link from 'next/link'
 import Date from '../components/date'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
+  const allWorkData = getSortedWorkData()
   return {
     props: {
-      allPostsData
+      allPostsData,
+      allWorkData
     }
   }
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ allPostsData, allWorkData }) {
   return (
     <Layout home>
       <Head>
@@ -23,6 +26,22 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Hi! My name is Brennan and I'm a Software Engineer at PBS NewsHour.</p>
         <p>Creativity is my specialty, and I express it through my work on websites, in problem solving, and even writing music.</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allWorkData.map(({ id, date, title, intro }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/work/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
--- pages/work/[id].js
+++ pages/work/[id].js
@@ -1,39 +1,39 @@
 import Layout from '../../components/layout'
-import { getAllPostIds, getPostData } from '../../lib/posts'
+import { getAllWorkIds, getWorkData } from '../../lib/work'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.scss'
 
 
 export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
+  const workData = await getWorkData(params.id)
   return {
     props: {
-      postData
+      workData
     }
   }
 }
 
 export async function getStaticPaths() {
-  const paths = getAllPostIds()
+  const paths = getAllWorkIds()
   return {
     paths,
     fallback: false
   }
 }
 
-export default function Post({ postData }) {
+export default function Work({ workData }) {
   return (
     <Layout>
       <Head>
-        <title>{postData.title}</title>
+        <title>{workData.title}</title>
       </Head>
 
-      <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+      <h1 className={utilStyles.headingXl}>{workData.title}</h1>
       <div className={utilStyles.lightText}>
-        <Date dateString={postData.date} />
+        <Date dateString={workData.date} />
       </div>
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <div dangerouslySetInnerHTML={{ __html: workData.contentHtml }} />
     </Layout>
   )
 }
